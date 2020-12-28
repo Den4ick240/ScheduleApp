4c394a9885ad809db0cc88422d2e5855dbfdb2c1
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2020-12-28 17:52:27 +0700

    Add files via upload

---
+++ src/main/java/ru/nsu/ccfit/zhigalov/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package ru.nsu.ccfit.zhigalov.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import ru.nsu.ccfit.zhigalov.myapplication.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int[] TAB_TITLES = {
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3,
+            R.string.tab_text_4,
+            R.string.tab_text_5,
+            R.string.tab_text_6,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager(), TAB_TITLES);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+}
